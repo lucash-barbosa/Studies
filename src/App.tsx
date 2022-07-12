74ff329a6362111d0c69f54f4dc5d2ff6894bb19
Lucaslkj <lucas565lkj@gmail.com>
2022-07-12 14:29:39 -0300

    Criando formulário e lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Formulario from './components/Formulario';
+import Lista from './components/Lista'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+     <div className="App">
+       <Formulario />
+       <Lista />
+     </div>
   );
-}
+ }
 
-export default App;
+ export default App;
---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Botao from '../Botao';
+
+class Formulario extends React.Component {
+  render() {
+    return (
+        <form>
+          <div>
+            <label htmlFor="tarefa">
+                Adicione um novo estudo
+              </label>
+              <input
+                type="text"
+                name="tarefa"
+                id="tarefa"
+                placeholder="O que você quer estudar"
+                required
+              /> 
+          </div>
+          <div>
+            <label htmlFor='tempo'>
+              Tempo
+            </label>
+            <input
+                type="time"
+                name="1"
+                id="tempo"
+                min="00:00:00"
+                max="01:30:00"
+                required
+              /> 
+          </div>
+        <Botao />                
+      </form>
+    )
+  }
+}
+
+export default Formulario;
