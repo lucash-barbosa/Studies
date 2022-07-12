fd567bff8ee1471c412b7cf9ed5fc3f8c94bdc08
Lucaslkj <lucas565lkj@gmail.com>
2022-07-12 15:49:23 -0300

    Aplicando css modules

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.scss';
+import style from './Lista.module.scss';
 
 function Lista() {
     const tarefas = [{
@@ -10,11 +10,11 @@ function Lista() {
         tempo: '01:00:00'
     }]
     return (
-        <aside className='listaTarefas'>
+        <aside className={style.listatarefas}>
             <h2> Estudos do dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index} className="item">
+                    <li key={index} className={style.item}>
                         <h3> {item.tarefa} </h3>
                         <span> {item.tempo} </span>
                     </li>
