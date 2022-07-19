7a5942045207c2ebf5005d3409c888fc17dd3c18
Lucaslkj <lucas565lkj@gmail.com>
2022-07-19 18:15:55 -0300

    Adicionando state

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { ITarefa } from '../../types/tarefa';
 import Botao from '../Botao';
 import style from './Formulario.module.scss';
+import { v4 as uuidv4 } from 'uuid';
 
-class Formulario extends React.Component {
+class Formulario extends React.Component<{
+  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+  }> {
+  state = {
+    tarefa: "",
+    tempo: "00:00:00"
+  }
+
+  adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    this.props.setTarefas(tarefasAntigas => 
+      [
+        ...tarefasAntigas,
+        {
+          ...this.state,
+          selecionado: false,
+          completado: false,
+          id: uuidv4()
+        }
+      ]
+    );
+    console.log('state', this.state)
+    this.setState({
+      tarefa: "",
+      tempo: "00:00"
+    })
+  }
+
   render() {
     return (
-        <form className={style.novaTarefa}>
+        <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
           <div className={style.inputContainer}>
             <label htmlFor="tarefa">
                 Adicione um novo estudo
@@ -14,6 +43,8 @@ class Formulario extends React.Component {
                 type="text"
                 name="tarefa"
                 id="tarefa"
+                value={this.state.tarefa}
+                onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
                 placeholder="O que você quer estudar"
                 required
               /> 
@@ -26,12 +57,14 @@ class Formulario extends React.Component {
                 type="time"
                 name="1"
                 id="tempo"
+                value={this.state.tempo}
+                onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
                 min="00:00:00"
-                max="01:30:00"
+                max="24:00:00"
                 required
               /> 
           </div>
-        <Botao> Adicionar </Botao>             
+        <Botao type="submit"> Adicionar </Botao>             
       </form>
     )
   }
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,14 +1,9 @@
-import Item from './item';
+import { ITarefa } from '../../types/tarefa';
+import Item from './Item';
 import style from './Lista.module.scss';
 
-function Lista() {
-    const tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    }, {
-        tarefa: 'JavaScript',
-        tempo: '01:00:00'
-    }]
+
+function Lista({ tarefas }: { tarefas: ITarefa[]}) {
     return (
         <aside className={style.listatarefas}>
             <h2> Estudos do dia </h2>
